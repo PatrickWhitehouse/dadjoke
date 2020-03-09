5bc53b0f750fea6379767201ef1650dc9cbbfb42
Patrick Whitehouse <patrick1996@gmail.com>
2020-03-09 19:32:25 +0000

    begin joke course

---
+++ jokes.js
@@ -0,0 +1,29 @@
+const jokeButton = document.querySelector(".getJoke");
+const jokeHolder = document.querySelector(".getJoke p");
+const buttonText = [
+  "Ugh.",
+  "🤦🏻‍♂️",
+  "omg dad.",
+  "you are the worst",
+  "seriously",
+  "stop it.",
+  "please stop",
+  "that was the worst one"
+];
+
+async function fetchJoke() {
+  const response = await fetch("https://icanhazdadjoke.com", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  console.log(joke);
+}
+
+jokeButton.addEventListener("click", handleClick);
